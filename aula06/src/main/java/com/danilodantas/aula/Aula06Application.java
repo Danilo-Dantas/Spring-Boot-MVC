aacccf00ce6272f6eb086f17eb478dae2cc2363e
Danilo <danilodantasofc@gmail.com>
2023-01-16 16:39:14 -0300

    Listando dados de uma entidade usando thymeleaf

---
+++ aula06/src/main/java/com/danilodantas/aula/Aula06Application.java
@@ -0,0 +1,14 @@
+package com.danilodantas.aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula06Application.class, args);
+	}
+
+}
+
